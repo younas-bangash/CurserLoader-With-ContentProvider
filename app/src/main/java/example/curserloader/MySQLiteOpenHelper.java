7656c7c0d0bdb9de5cf8e46ac1398d4036a5b32e
Younas <engr.younasbangash@gmail.com>
2017-02-27 00:53:26 +0400

    Inital Commit

---
+++ app/src/main/java/example/curserloader/MySQLiteOpenHelper.java
@@ -0,0 +1,58 @@
+package example.curserloader;
+
+/**
+ * Created by Younas on 2/27/2017.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public static final String LOG_TAG = MySQLiteOpenHelper.class.getSimpleName();
+
+    /** Name of the database file */
+    private static final String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link MySQLiteOpenHelper}.
+     *
+     * @param context of the app
+     */
+    public MySQLiteOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // Create a String that contains the SQL statement to create the pets table
+        String SQL_CREATE_PETS_TABLE =  "CREATE TABLE " + PetContract.PetEntry.TABLE_NAME + " ("
+                + PetContract.PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetContract.PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetContract.PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetContract.PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetContract.PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+
+        // Execute the SQL statement
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+    }
+
+    /**
+     * This is called when the database needs to be upgraded.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // The database is still at version 1, so there's nothing to do be done here.
+    }
+
+}
--- app/src/main/java/example/curserloader/Provider.java
+++ app/src/main/java/example/curserloader/Provider.java
@@ -1,8 +1,11 @@
 package example.curserloader;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -14,16 +17,30 @@ public class Provider extends ContentProvider {
     public static final String CONTENT_AUTHORITY = "example.curserloader";
     public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
     public static final String PATH_PETS = "pets";
+    public static final int PETS = 100;
+    public static final int PET_ID = 101;
     public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+    private MySQLiteOpenHelper mDbHelper;
+
+    // Creates a UriMatcher object.
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    static {
+        sUriMatcher.addURI(CONTENT_AUTHORITY, "pets", PETS);
+        sUriMatcher.addURI(CONTENT_AUTHORITY, "pets/#", PET_ID);
+    }
 
     @Override
     public boolean onCreate() {
+        mDbHelper = new MySQLiteOpenHelper(getContext());
         return false;
     }
 
     @Nullable
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+
         return null;
     }
 
@@ -40,12 +57,54 @@ public class Provider extends ContentProvider {
     }
 
     @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+            case PET_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return database.delete(PetContract.PetEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
     }
 
     @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+    public int update(Uri uri, ContentValues contentValues, String selection,
+                      String[] selectionArgs) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            case PET_ID:
+                // For the PET_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return updatePet(uri, contentValues, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Update pets in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more pets).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updatePet(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        // TODO: Update the selected pets in the pets database table with the given ContentValues
+
+        // TODO: Return the number of rows that were affected
         return 0;
     }
 }
